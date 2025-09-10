97b0b772d22838e8ed11bb6f8a42f8bc8e703082
Archit3ct <timothy.s.rasmussen@gmail.com>
2025-09-10 10:53:11 +0200

    update

--- databases/project-1/queries/T3_get_courses_registered_by_student_30397.sql
+++ databases/project-1/queries/T3_get_courses_registered_by_student_30397.sql
@@ -1,6 +1,9 @@
 -- phase 1
 
-
+-- i get all the info of the courses taken by the studetn '79352'
+-- there is a bad practise here it is not defined it is a student id that is takes.id
+-- should be takes.student_id
+select * from takes where takes.id = '30397';
 
 --
 
