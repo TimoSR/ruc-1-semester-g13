cb0fbad6182bccceb39d91fd74828e4d626cb0cb
Archit3ct <timothy.s.rasmussen@gmail.com>
2025-09-10 10:55:06 +0200

    update

--- databases/project-1/queries/T8_name_instructors_not_just_ID.sql
+++ databases/project-1/queries/T8_name_instructors_not_just_ID.sql
@@ -1,3 +1,13 @@
+-- phase 1
+
+SELECT * from instructor;
+
+-- phase x
+
+-- ....
+
+-- phase 2
+
 SELECT instructor.ID, "name"
 FROM instructor 
 WHERE instructor.dept_name = 'Marketing'
@@ -6,3 +16,19 @@ WHERE instructor.dept_name = 'Marketing'
     FROM teaches 
     WHERE teaches."id"= instructor.ID
   );
+
+  -- phase 3 (optimizing readabilit)(DONE)
+
+  WITH InstructorsWhoHaveTaught AS (
+  SELECT DISTINCT teaches.id
+  FROM teaches
+),
+MarketingInstructorsWhoNeverTaught AS (
+  SELECT instructor.ID, instructor.name
+  FROM instructor
+  WHERE instructor.dept_name = 'Marketing'
+    AND instructor.ID NOT IN (SELECT id FROM InstructorsWhoHaveTaught)
+)
+
+SELECT *
+FROM MarketingInstructorsWhoNeverTaught;
