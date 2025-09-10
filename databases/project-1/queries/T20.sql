8f00c02b823891288d3ea80e48ace0b8c8eab77f
Chiara <chiaravisca@outlook.com>
2025-09-10 13:35:24 +0200

    improve T20

--- databases/project-1/queries/T20.sql
+++ databases/project-1/queries/T20.sql
@@ -1,10 +1,11 @@
 WITH section_count AS (
-    SELECT  name, COUNT(DISTINCT sec_id) as sec_cnt
+    SELECT  id, COUNT(DISTINCT sec_id) as sec_cnt
     FROM teaches as t
     JOIN instructor as i ON t.id = i.id
-    GROUP BY name
+    GROUP BY id
 )
 UPDATE instructor
 SET salary = 29001 + (10000 * section_count.sec_cnt)
 FROM section_count
 WHERE instructor.id = section_count.id;
+
