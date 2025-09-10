6332561cb9fc5f444d79934815de2491ba8daf67
Chiara <chiaravisca@outlook.com>
2025-09-10 13:51:46 +0200

    fixed T20

--- databases/project-1/queries/T20.sql
+++ databases/project-1/queries/T20.sql
@@ -1,11 +1,24 @@
 WITH section_count AS (
-    SELECT  id, COUNT(DISTINCT sec_id) as sec_cnt
-    FROM teaches as t
-    JOIN instructor as i ON t.id = i.id
-    GROUP BY id
+    SELECT inst.id, COUNT(tea.sec_id) AS sec_cnt
+    FROM instructor AS inst
+    LEFT JOIN teaches AS tea ON inst.id = tea.id
+    GROUP BY inst.id
 )
-UPDATE instructor
-SET salary = 29001 + (10000 * section_count.sec_cnt)
-FROM section_count
-WHERE instructor.id = section_count.id;
+UPDATE instructor AS i
+SET salary = 29001 + (10000 * s.sec_cnt)
+FROM section_count AS s
+WHERE i.id = s.id;
 
+-- SELECT QUERY WITH UPDATED SALARIES TO VERIFY CORRECT UPDATE:
+
+-- WITH section_count AS (
+--     SELECT inst.id, COUNT(tea.sec_id) AS sec_cnt
+--     FROM instructor AS inst
+--     LEFT JOIN teaches AS tea ON inst.id = tea.id
+--     GROUP BY inst.id
+-- )
+-- SELECT i.id, i.name, s.sec_cnt, i.salary,
+--        29001 + (10000 * s.sec_cnt) AS new_salary
+-- FROM instructor AS i
+-- JOIN section_count AS s ON i.id = s.id
+-- ORDER BY i.name;
