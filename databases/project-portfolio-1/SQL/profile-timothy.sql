93b52dba09a34ce9f0f1b260cd4d34efbc6c7673
TimoSR <timothy.s.rasmussen@gmail.com>
2025-09-27 11:20:13 +0200

    update

--- databases/project-portfolio-1/SQL/profile-timothy.sql
+++ databases/project-portfolio-1/SQL/profile-timothy.sql
@@ -1,6 +1,3 @@
--- ===========================
--- USERS & PROFILES
--- ===========================
 CREATE TABLE IF NOT EXISTS profile (
     id SERIAL PRIMARY KEY,
     username VARCHAR(50) NOT NULL UNIQUE,
