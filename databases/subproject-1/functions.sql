a730d9e5b8eb0f87cb5da1e144dce1ba6d5326f9
Chiara <chiaravisca@outlook.com>
2025-09-29 17:16:04 +0200

    fixed pgsql syntax

--- databases/subproject-1/functions.sql
+++ databases/subproject-1/functions.sql
@@ -7,7 +7,9 @@ RETURNS TABLE (
     person_id VARCHAR(10),
     primary_name TEXT,
     frequency INT
-) AS $$
+)
+LANGUAGE plpgsql 
+AS $$
 BEGIN
     RETURN QUERY
     -- find the person_id of actor from their name
@@ -37,7 +39,7 @@ BEGIN
     JOIN person p ON p.id = c.person_id
     ORDER BY c.freq DESC, p.primary_name;
 END;
-$$ LANGUAGE plpgsql;
+$$;
 
 -- 1-D.7 Name rating
 
@@ -46,16 +48,17 @@ $$ LANGUAGE plpgsql;
 -- but that can be added later in case!
 
 CREATE TABLE person_rating (
-    person_id VARCHAR(10) PRIMARY KEY,
-    weighted_rating NUMERIC(3,2),
-    FOREIGN KEY (person_id) REFERENCES person(id) ON DELETE CASCADE
+    person_id VARCHAR(10) PRIMARY KEY REFERENCES person(id) ON DELETE CASCADE,
+    weighted_rating NUMERIC(3,2)
 );
 
 -- this data is stored in its own table (not to pollute the rest of the DB with data that won't be used frequently)
 -- so to access it, we have to query the table by joining it with person or actor in case we need other info on the actors
 
 CREATE OR REPLACE FUNCTION calculate_actor_ratings()
-RETURNS void AS $$
+RETURNS void
+LANGUAGE plpgsql 
+AS $$
 BEGIN
     -- clear old ratings
     DELETE FROM person_rating;
@@ -69,7 +72,7 @@ BEGIN
     JOIN rating r ON a.title_id = r.title_id
     GROUP BY a.person_id;
 END;
-$$ LANGUAGE plpgsql;
+$$;
 
 -- 1-D.8 Popular actors
 
@@ -85,7 +88,9 @@ RETURNS TABLE (
     actor_id VARCHAR(10),
     actor_name TEXT,
     weighted_rating NUMERIC(3,2)
-) AS $$
+)
+LANGUAGE plpgsql 
+AS $$
 BEGIN
     RETURN QUERY
     WITH target AS (
@@ -110,7 +115,7 @@ BEGIN
     JOIN target t ON t.actor_id <> a.person_id
     ORDER BY pr.weighted_rating DESC NULLS LAST, p.primary_name;
 END;
-$$ LANGUAGE plpgsql;
+$$;
 
 
 -- 1-D.11 Exact-match querying
@@ -121,7 +126,9 @@ CREATE OR REPLACE FUNCTION exact_match_query(keywords TEXT[])
 RETURNS TABLE (
     title_id VARCHAR(10),
     primary_title TEXT
-) AS $$
+)
+LANGUAGE plpgsql 
+AS $$
 BEGIN
     RETURN QUERY
     SELECT t.id, t.primary_title
@@ -135,7 +142,7 @@ BEGIN
     )
     ORDER BY t.primary_title;
 END;
-$$ LANGUAGE plpgsql;
+$$;
 -- to use it: SELECT * FROM exact_match_query(ARRAY['star', 'wars']);
 
 -- 1-D.12 Best-match querying
@@ -149,7 +156,9 @@ RETURNS TABLE (
     primary_title TEXT,
     -- the higher the match_count, the higher the rank
     match_count INT
-) AS $$
+)
+LANGUAGE plpgsql 
+AS $$
 BEGIN
     RETURN QUERY
     SELECT 
@@ -163,7 +172,7 @@ BEGIN
     -- no HAVING clause, so it returns titles even if only some keywords are matched
     ORDER BY match_count DESC, t.primary_title;
 END;
-$$ LANGUAGE plpgsql;
+$$;
 
 -- same usage as 11
 
@@ -176,7 +185,9 @@ CREATE OR REPLACE FUNCTION word_to_words_query(keywords TEXT[])
 RETURNS TABLE (
     word TEXT,
     frequency INT
-) AS $$
+)
+LANGUAGE plpgsql 
+AS $$
 BEGIN
     RETURN QUERY
     WITH matching_titles AS (
@@ -199,7 +210,7 @@ BEGIN
     -- 20 for simplicity, there would be too many results without limit
     LIMIT 20;
 END;
-$$ LANGUAGE plpgsql;
+$$;
 
 -- same usage as 11, 12
 
